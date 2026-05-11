24cc5a4cdde3063d5b8aa5779a9a019460d48682
ViibhaBShankar <vvce23ise0073@vvce.ac.in>
2026-05-11 15:35:03 +0530

    First project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
+
 import "./App.css";
 
-import { getPosts } from "./services/api";
+import { getPosts } from "./api";
 
 import Card from "./components/Card";
-import Loader from "./components/Loader";
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -13,39 +13,25 @@ function App() {
 
   const [error, setError] = useState("");
 
-  const [search, setSearch] = useState("");
-
   useEffect(() => {
     getPosts()
       .then((response) => {
         setPosts(response.data);
+
         setLoading(false);
       })
+
       .catch((error) => {
         console.log(error);
 
-        setError("Failed to fetch data");
+        setError("Error fetching data");
 
         setLoading(false);
       });
   }, []);
 
-  const handleDelete = (id) => {
-    const updatedPosts = posts.filter(
-      (item) => item.id !== id
-    );
-
-    setPosts(updatedPosts);
-  };
-
-  const filteredPosts = posts.filter((item) =>
-    item.title
-      .toLowerCase()
-      .includes(search.toLowerCase())
-  );
-
   if (loading) {
-    return <Loader />;
+    return <h1>Loading...</h1>;
   }
 
   if (error) {
@@ -54,30 +40,17 @@ function App() {
 
   return (
     <div className="container">
-      <h1 className="heading">
-        Axios API Project
-      </h1>
+      <h1>API Posts</h1>
 
-      <input
-        type="text"
-        placeholder="Search posts..."
-        className="search-input"
-        onChange={(e) => setSearch(e.target.value)}
-      />
-
-      <div className="card-container">
-        {filteredPosts.map((item) => {
-          return (
-            <Card
-              key={item.id}
-              id={item.id}
-              title={item.title}
-              body={item.body}
-              onDelete={handleDelete}
-            />
-          );
-        })}
-      </div>
+      {posts.slice(0, 6).map((item) => {
+        return (
+          <Card
+            key={item.id}
+            title={item.title}
+            body={item.body}
+          />
+        );
+      })}
     </div>
   );
 }
--- src/services/Api.jsx
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API_URL =
-  "https://jsonplaceholder.typicode.com/posts";
-
-export const getPosts = () => {
-  return axios.get(API_URL);
-};
